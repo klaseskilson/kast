5feeb33d9ff2e0c126ac26409404129bae9a267d
Klas Eskilson <klas.eskilson@gmail.com>
2016-04-12 09:21:40 +0200

    Refactor debounce call in SettingsPage

--- imports/ui/pages/SettingsPage.jsx
+++ imports/ui/pages/SettingsPage.jsx
@@ -6,11 +6,6 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Spinner } from '../components/common.jsx';
 import { updateUser, setUsername } from '../../api/users/methods.js';
 
-const debouncedUpdate = _.debounce((method, params, onStart, onFinished) => {
-  onStart();
-  method.call(params, onFinished);
-}, 1000);
-
 class SettingsPage extends Component {
   constructor(props) {
     super(props);
@@ -19,6 +14,11 @@ class SettingsPage extends Component {
     this.startUpdate = this.startUpdate.bind(this);
     this.finishedUpdate = this.finishedUpdate.bind(this);
     this.handleUsername = this.handleUsername.bind(this);
+
+    this.update = _.debounce((method, params) => {
+      this.startUpdate();
+      method.call(params, this.finishedUpdate);
+    }, 1000);
   }
 
   handleChange(event) {
@@ -30,20 +30,19 @@ class SettingsPage extends Component {
     this.setState({ user });
 
     // avoid clogging server with method calls, update only once
-    debouncedUpdate(updateUser, {
-      key: name,
-      value,
-    }, this.startUpdate, this.finishedUpdate);
+    this.update(updateUser, { key: name, value });
   }
 
   handleUsername(event) {
     const user = this.state.user;
     const { value } = event.target;
 
-    // use Lodash's update to update object by using a `nested.key.like.this`
+    // update state so that change feels instant
     user.username = value;
     this.setState({ user });
-    debouncedUpdate(setUsername, value, this.startUpdate, this.finishedUpdate);
+
+    // make debounced call to not be clogging server with calls
+    this.update(setUsername, value);
   }
 
   startUpdate() {
@@ -54,22 +53,15 @@ class SettingsPage extends Component {
   }
 
   finishedUpdate(error) {
-    if (error) {
-      this.setState({
-        message: error.message,
-        loading: false,
-      });
-    } else {
-      this.setState({
-        message: 'All changes saved!',
-        loading: false,
-      });
-    }
+    this.setState({
+      message: error && error.message || 'All changes saved!',
+      loading: false,
+    });
   }
 
   render() {
     const { user, loading, message } = this.state;
-    const profileName = user.profile && user.profile.name || '';
+    const profileName = user && user.profile && user.profile.name || '';
     return (
       <div>
         <h1>
@@ -78,8 +70,12 @@ class SettingsPage extends Component {
         { message ? (<p>{message}</p>) : ''}
         { user ? (
           <form onSubmit={event => event.preventDefault()}>
-            <input value={profileName} name="profile.name" placeholder="Your name..." onChange={this.handleChange} />
-            <input value={user.username || ''} name="username" placeholder="Username..." onChange={this.handleUsername} />
+            <input value={profileName} name="profile.name"
+              placeholder="Your name..." onChange={this.handleChange}
+            />
+            <input value={user.username || ''}
+              placeholder="Username..." onChange={this.handleUsername}
+            />
           </form>
         ) : 'loading...'}
         {JSON.stringify(user)}
