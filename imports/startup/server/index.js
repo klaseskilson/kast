6af6327cd3f2f103c26131b38e1b53a603431bc7
Klas Eskilson <klas.eskilson@gmail.com>
2016-04-14 09:53:04 +0200

    Add index for searchUrl in SearchCache

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -5,3 +5,24 @@ import './ServiceConfiguration.js';
 // as an API to the client.
 import '../../api/api.js';
 import '../../api/SearchCache/server/cacheChecker.js';
+
+// add indexes to mongo
+import { Meteor } from 'meteor/meteor';
+import SearchCache from '../../api/SearchCache/SearchCache.js';
+
+Meteor.startup(() => {
+  /* eslint-disable no-console */
+  console.info('Creating indexes for collections...');
+  SearchCache.rawCollection().createIndex({
+    searchUrl: 1,
+  }, {
+    background: 1,
+  }, (error, indexName) => {
+    if (error) {
+      console.error('When index creation attempt:', error);
+    } else {
+      console.info('... created index for SearchCache:', indexName);
+    }
+  });
+  /* eslint-enable */
+});
