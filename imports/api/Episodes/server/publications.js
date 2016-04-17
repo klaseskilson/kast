399a23f96dda7e36fe14203eba390340b8b1828b
Klas Eskilson <klas.eskilson@gmail.com>
2016-04-17 22:59:24 +0200

    Sort Episodes publication cursor by publish date

--- imports/api/Episodes/server/publications.js
+++ imports/api/Episodes/server/publications.js
@@ -5,7 +5,11 @@ import Episodes from '../Episodes.js';
 Episodes.pubs = {};
 
 Meteor.publish('Episodes.pubs.all', podcastId => {
-  const search = Episodes.find({ podcastId });
+  const search = Episodes.find({ podcastId }, {
+    sort: {
+      published: -1,
+    },
+  });
 
   if (search.count() === 0) {
     Episodes.methods.import.call(podcastId);
