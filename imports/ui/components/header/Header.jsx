d33d525707b7b40622bcb675a594dd6132fe6cca
Klas Eskilson <klas.eskilson@gmail.com>
2016-05-10 16:25:23 +0200

    Add detached class to header

--- imports/ui/components/header/Header.jsx
+++ imports/ui/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
@@ -10,17 +10,40 @@ import { Container } from '../common.jsx';
 import header from './Header.mss';
 import menu from './Menu.mss';
 
-const Header = ({ currentRoute }) => (
-  <header className={header.mainHeader}>
-    <Container extraClass={menu.container}>
-      <MainMenu currentRoute={currentRoute} />
-      <div className={menu.container}>
-        <SearchForm />
-        <UserMenu currentRoute={currentRoute} />
-      </div>
-    </Container>
-  </header>
-);
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isAtTop: true };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    this.setState({ isAtTop: window.scrollY === 0 });
+  }
+
+  render() {
+    const { currentRoute } = this.props;
+    const scrollClass = this.state.isAtTop ? header.top : header.detached;
+    return (
+      <header className={`${header.mainHeader} ${scrollClass}`}>
+        <Container extraClass={menu.container}>
+          <MainMenu currentRoute={currentRoute} />
+          <div className={menu.container}>
+            <SearchForm />
+            <UserMenu currentRoute={currentRoute} />
+          </div>
+        </Container>
+      </header>
+    );
+  }
+}
 
 Header.propTypes = {
   currentRoute: PropTypes.object.isRequired,
--- imports/ui/components/header/Header.mss
+++ imports/ui/components/header/Header.mss
@@ -1,7 +1,6 @@
 @import 'client/stylesheets/includes/_variables.scss';
 
 .mainHeader {
-  background: rgba(#fff, 0.8);
   padding: 0;
   width: 100%;
   position: fixed;
@@ -9,4 +8,14 @@
   left: 0;
   z-index: 100;
   line-height: 1;
+
+  &.top {
+    a {
+      color: #fff;
+    }
+  }
+  &.detached {
+    background: rgba(#fff, 0.8);
+  }
 }
+
