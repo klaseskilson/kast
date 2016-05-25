b19cce6059cbef1c24b6161d3fd0eaceb9f5aac7
Klas Eskilson <klas.eskilson@gmail.com>
2016-05-25 17:53:30 +0200

    Add user schema

---
+++ imports/api/users/schema.js
@@ -0,0 +1,100 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+const Schema = {};
+
+const UserCountry = new SimpleSchema({
+  name: {
+    type: String
+  },
+  code: {
+    type: String,
+    regEx: /^[A-Z]{2}$/
+  }
+});
+
+const UserProfile = new SimpleSchema({
+  firstName: {
+    type: String,
+    optional: true
+  },
+  lastName: {
+    type: String,
+    optional: true
+  },
+  birthday: {
+    type: Date,
+    optional: true
+  },
+  gender: {
+    type: String,
+    allowedValues: ['Male', 'Female'],
+    optional: true
+  },
+  organization : {
+    type: String,
+    optional: true
+  },
+  website: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Url,
+    optional: true
+  },
+  bio: {
+    type: String,
+    optional: true
+  },
+  country: {
+    type: Schema.UserCountry,
+    optional: true
+  }
+});
+
+const UserSchema = new SimpleSchema({
+  username: {
+    type: String,
+    // For accounts-password, either emails or username is required, but not both.
+    // It is OK to make this optional here because the accounts-password package
+    // does its own validation. Third-party login packages may not require either.
+    // Adjust this schema as necessary for your usage.
+    optional: true,
+  },
+  emails: {
+    type: Array,
+    // For accounts-password, either emails or username is required, but not both.
+    // It is OK to make this optional here because the accounts-password package does
+    // its own validation. Third-party login packages may not require either. Adjust
+    // this schema as necessary for your usage.
+    optional: true,
+  },
+  'emails.$': {
+    type: Object,
+  },
+  'emails.$.address': {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email,
+  },
+  'emails.$.verified': {
+    type: Boolean,
+  },
+  createdAt: {
+    type: Date,
+  },
+  profile: {
+    type: Schema.UserProfile,
+    optional: true,
+  },
+  // Make sure this services field is in your schema if you're using any of the accounts packages
+  services: {
+    type: Object,
+    optional: true,
+    blackbox: true,
+  },
+  // In order to avoid an 'Exception in setInterval callback' from Meteor
+  heartbeat: {
+    type: Date,
+    optional: true,
+  },
+
+  podcastSubscriptions: { type: [String], defaultValue: [] },
+});
+
+export default UserSchema;
